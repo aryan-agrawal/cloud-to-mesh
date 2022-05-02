7b0e46e6b3e0a142cb7e59c3411a5d61d9312c20
Tarun Singh <tarunsingh@berkeley.edu>
2022-05-02 11:21:03 -0700

    implemented find_candidate not tested

--- src/mesh_algorithms/ball_pivot.py
+++ src/mesh_algorithms/ball_pivot.py
@@ -44,11 +44,8 @@ def find_seed_triangle(radius, unused):
                 if (v1 == v) or (v2 == v) or (v1 == v2):
                     continue
                 
-                pt1 = v.coord
-                pt2 = v1.coord
-                pt3 = v2.coord
-                vector1 = pt2-pt1
-                vector2 = pt3-pt1
+                vector1 = v1.coord - v.coord
+                vector2 = v2.coord - v.coord
                 # get normal of triangle
                 triangle_normal = np.cross(vector1, vector2)
 
@@ -56,46 +53,23 @@ def find_seed_triangle(radius, unused):
                 candidate = Triangle((v, v1, v2), triangle_normal)
 
                 # check that triangle normal is consistent with all 3 vertex normals
-                check1 = np.dot(triangle_normal, v.normal)
-                check2 = np.dot(triangle_normal, v1.normal)
-                check3 = np.dot(triangle_normal, v2.normal)
-                if (check1 <= 0) or (check2 <= 0) or (check3 <= 0):
+                if not is_compatible(v, v1, v2):
                     continue
 
                 # test that the r-ball touches all 3 vertices and no other data point
-                a = v.coord_distance(v1)  # a = v2
-                b = v1.coord_distance(v2) # b = v
-                c = v2.coord_distance(v) # c = v1
-                h_x = a**2 * (b**2 + c**2 - a**2)
-                h_y = b**2 * (a**2 + c**2 - b**2) 
-                h_z = c**2 * (a**2 + b**2 - c**2)
-
-                # normalize barycentric coords
-                sum = h_x + h_y + h_z
-                h_x /= sum
-                h_y /= sum
-                h_z /= sum 
-                h = h_x * v2.coord + h_y * v.coord + h_z * v1.coord
-
-                r_2 = (a**2 * b**2 * c**2) / ((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c))
-                if (radius ** 2 - r_2) < 0:
-                    # ball doesn't pass radius check 
+                sphere_center = compute_ball_center(radius, v, v1, v2)
+                if sphere_center is None:
                     continue
-                sphere_center = h + (np.sqrt(radius**2 - r_2) * (triangle_normal / np.linalg.norm(triangle_normal)))
+
+                # set triangle's circumcenter
+                candidate.circumcenter = sphere_center
 
                 # check that sphere doesn't contain any other points 
                 # check the neighborhood of the center of the sphere
-                sphere_vertex = Vertex(-1, sphere_center, None)
-                sphere_neighbors = get_neighbors(sphere_vertex, radius)
-                valid_candidate = True
-                for u in sphere_neighbors:
-                    if u.coord_distance(sphere_vertex) < radius:
-                        valid_candidate = False
-                        break
-                if not(valid_candidate):
+                if not is_empty_ball(v, v1, v2, sphere_center, radius):
                     continue
-                else:
-                    return candidate, sphere_center
+
+                return candidate, sphere_center
     return None
 
 def get_neighbors(vertex, radius):
@@ -129,7 +103,9 @@ def bpa(radius):
     unused_vertices = set(vertices) # constant time additions and deletions
     triangle_mesh = []
     while (unused_vertices):
-        seed_tri, _ = find_seed_triangle(radius, unused_vertices)
+        seed_tri, ball_center = find_seed_triangle(radius, unused_vertices)
+        print("seed tri: ", [v.index for v in seed_tri.vertices])
+        # visualize(radius, seed_tri, ball_center)
         if seed_tri is None:
             break
             #return
@@ -139,8 +115,9 @@ def bpa(radius):
         unused_vertices.remove(seed_tri.vertices[2])
         triangle_mesh += [seed_tri]
         # expand triangulation
-        updated_mesh = expand_triangulation(triangle_mesh, seed_tri, radius)
-
+        updated_mesh, new_center = expand_triangulation(triangle_mesh, seed_tri, radius)
+        visualize(radius, [seed_tri], [ball_center, new_center])
+        return
 
         
         # print(seed_tri.vertices)
@@ -153,15 +130,31 @@ def expand_triangulation(triangle_mesh, seed_tri, radius):
     edge1 = Edge(seed_tri.vertices[1], seed_tri.vertices[2], Edge.EdgeType.FRONT, [seed_tri])
     edge2 = Edge(seed_tri.vertices[2], seed_tri.vertices[0], Edge.EdgeType.FRONT, [seed_tri])
     edge_front += [edge0, edge1, edge2]
+
+    print("finding edge0 candidates: \n")
+    new_vertex, new_center = find_candidate(edge0, radius)
+    if new_vertex:
+        print("new candidate 1: ", new_vertex.index, new_vertex.coord, new_center)
+    print("finding edge1 candidates: \n")
+    new_vertex, new_center = find_candidate(edge1, radius)
+    if new_vertex:
+        print("new candidate 2: ", new_vertex.index, new_vertex.coord, new_center)
+    print("finding edge2 candidates: \n")
+    new_vertex, new_center = find_candidate(edge2, radius)
+    if new_vertex:
+        print("new candidate 3: ", new_vertex.index, new_vertex.coord, new_center)
+    return new_vertex, new_center
+
+
     while len(edge_front) > 0:
         # pop from the front
         curr_edge = edge_front.pop(0)
         if curr_edge.edge_type == Edge.EdgeType.BOUNDARY or curr_edge.edge_type == Edge.EdgeType.FROZEN:
             continue
-        new_vertex = find_candidate(curr_edge, radius)
+        new_vertex, center = find_candidate(curr_edge, radius)
         if new_vertex is None:
             curr_edge.edge_type = Edge.EdgeType.BOUNDARY
-        new_tri = Triangle(curr_edge.vertices[0], curr_edge.vertices[1], new_vertex) # check this order
+        new_tri = Triangle((curr_edge.vertices[0], curr_edge.vertices[1], new_vertex), circumcenter_pos=center) # check this order
         triangle_mesh.append(new_tri)
         # e_s is the edge linking source and new_vertex
         edge_s = None
@@ -197,7 +190,112 @@ def find_candidate(edge, radius):
     r_prime = np.linalg.norm(midpoint - circumcenter) + radius
     theta_min = 2 * np.pi
     
+    neighbors = get_neighbors(Vertex(-1, midpoint, None), radius)
+    candidate = None
+    candidate_center = None
+    for v in neighbors:
+        # if v is an inner vertex, or belongs to e
+        bad_vertex = False
+        for t in edge.triangles:
+            # print("v index: ", v.index)
+            # print("vertices: ", [v.index for v in t.vertices])
+            if v in t.vertices:
+                bad_vertex = True
+        if bad_vertex:
+            continue
+
+        # print("v index past the check: ", v.index)
+        # if v is not compatible with e
+        # if not is_compatible(edge.vertices[0], edge.vertices[1], v): # TODO: check ordering
+            # continue
+
+        print("was compatible: ", v.index)
+
+        new_center = compute_ball_center(radius, edge.vertices[0], edge.vertices[1], v)
+        if new_center is None:
+            continue
+
+        print("ball existed: ", v.index)
+
+        # compute angle theta
+        vec_b = new_center - midpoint
+        vec_b /= np.linalg.norm(vec_b)
+
+        vec_a = circumcenter - midpoint
+        vec_a /= np.linalg.norm(vec_a)
+
+        cosinus = np.dot(vec_a, vec_b)
+        cosinus = np.clip(cosinus, -1.0, 1.0)
+        theta = np.arccos(cosinus)
+
+        if np.dot(np.cross(vec_a, vec_b), edge.vertices[1].coord - edge.vertices[0].coord) < 0:
+            theta = 2 * np.pi - theta
+
+        if theta >= theta_min:
+            continue
+
+        if not is_empty_ball(edge.vertices[0], edge.vertices[1], v, new_center, radius):
+            continue
+
+        print("is empty ball: ", v.index)
+
+        theta_min = theta
+        candidate = v
+        candidate_center = new_center
+
+
+    return candidate, candidate_center
+
     pass
+
+
+def is_compatible(v, v1, v2):
+    vector1 = v1.coord - v.coord
+    vector2 = v2.coord - v.coord
+    triangle_normal = np.cross(vector1, vector2)
+    return (np.dot(v.normal, triangle_normal) > 0) and (np.dot(v1.normal, triangle_normal) > 0) and (np.dot(v2.normal, triangle_normal) > 0)
+
+
+def compute_ball_center(radius, v, v1, v2):
+    a = v.coord_distance(v1)  # a = v2
+    b = v1.coord_distance(v2) # b = v
+    c = v2.coord_distance(v) # c = v1
+    h_x = a**2 * (b**2 + c**2 - a**2)
+    h_y = b**2 * (a**2 + c**2 - b**2) 
+    h_z = c**2 * (a**2 + b**2 - c**2)
+
+    # normalize barycentric coords
+    sum = h_x + h_y + h_z
+    h_x /= sum
+    h_y /= sum
+    h_z /= sum 
+    h = h_x * v2.coord + h_y * v.coord + h_z * v1.coord
+
+    r_2 = (a**2 * b**2 * c**2) / ((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c))
+    if (radius ** 2 - r_2) < 0:
+        # ball doesn't pass radius check
+        print("r_2 radius / radius: ", r_2, radius**2)
+        return None
+
+    vector1 = v1.coord - v.coord
+    vector2 = v2.coord - v.coord
+    # get normal of triangle
+    triangle_normal = np.cross(vector1, vector2)
+    sphere_center = h + (np.sqrt(radius**2 - r_2) * (triangle_normal / np.linalg.norm(triangle_normal)))
+
+    return sphere_center
+
+
+def is_empty_ball(v, v1, v2, ball_center, radius):
+    center_vertex = Vertex(-1, ball_center, None)
+    neighbors = get_neighbors(center_vertex, radius)
+    for n in neighbors:
+        if (n==v) or (n==v1) or (n==v2):
+            continue
+        if center_vertex.coord_distance(n) <= radius:
+            return False
+    return True
+
 
 def create_voxels(r):
     """
@@ -250,33 +348,42 @@ def main(path_name):
 def write_output():
     pass
 
-def visualize(radius, triangle, ball_center):
+def visualize(radius, triangles, ball_centers):
     pcd = o3d.geometry.PointCloud()
     points = np.array([(v.coord[0], v.coord[1], v.coord[2]) for v in vertices])
     color_mask = np.zeros((points.shape[0], 3))
 
     # color seed triangle vertices green
-    color_mask[triangle.vertices[0].index] = np.array([0, 1, 0])
-    color_mask[triangle.vertices[1].index] = np.array([0, 1, 0])
-    color_mask[triangle.vertices[2].index] = np.array([0, 1, 0])
+    for triangle in triangles:
+        color_mask[triangle.vertices[0].index] = np.array([0, 1, 0])
+        color_mask[triangle.vertices[1].index] = np.array([0, 1, 0])
+        color_mask[triangle.vertices[2].index] = np.array([0, 1, 0])
+
     pcd.points = o3d.utility.Vector3dVector(points)
     pcd.colors = o3d.utility.Vector3dVector(color_mask)
 
     line_set = o3d.geometry.LineSet()
     line_set.points = o3d.utility.Vector3dVector(points)
 
-    indices = [v.index for v in triangle.vertices]
-    edges = []
-    edges.append([indices[0], indices[1]])
-    edges.append([indices[1], indices[2]])
-    edges.append([indices[2], indices[0]])
+    for triangle in triangles:
+        indices = [v.index for v in triangle.vertices]
+        edges = []
+        edges.append([indices[0], indices[1]])
+        edges.append([indices[1], indices[2]])
+        edges.append([indices[2], indices[0]])
+
     line_set.lines = o3d.utility.Vector2iVector(edges)
 
     # draw ball
-    sphere = o3d.geometry.TriangleMesh.create_sphere(radius=radius)
-    sphere.translate((ball_center[0], ball_center[1], ball_center[2]))
+    spheres = []
+    for ball_center in ball_centers:
+        sphere = o3d.geometry.TriangleMesh.create_sphere(radius=radius)
+        sphere.translate((ball_center[0], ball_center[1], ball_center[2]))
+        spheres.append(sphere)
 
-    o3d.visualization.draw_geometries([pcd, line_set, sphere])
+    spheres += [pcd, line_set]
+
+    o3d.visualization.draw_geometries(spheres)
     return
 
     # Visualize voxels
--- src/mesh_algorithms/edge.py
+++ src/mesh_algorithms/edge.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import numpy as np
 
 class Edge:
